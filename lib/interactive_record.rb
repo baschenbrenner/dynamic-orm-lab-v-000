9e2e79abd9fa4c8d662fafed6541fc9d9b970af0
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-10-30 20:07:22 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -50,7 +50,7 @@ class InteractiveRecord
   end
 
   def self.find_by(attribute)
-    binding.pry
+    
 
     DB[:conn].execute("SELECT * FROM #{table_name} WHERE #{attribute.keys.first.to_s}='#{attribute.values.first}'")
   end
