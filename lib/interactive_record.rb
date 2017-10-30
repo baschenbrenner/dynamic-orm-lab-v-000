4dc4d040e517872e4c7b7c378aacd3a07d363128
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-10-30 19:38:23 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -39,6 +39,6 @@ class InteractiveRecord
   end
 
   def save
-    DB[:conn].execute("INSERT INTO #{table_name_for_insert} (name,grade) VALUES (?,?)", self.name, self.grade)
+    DB[:conn].execute("INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (?,?)", self.name, self.grade)
   end
 end
